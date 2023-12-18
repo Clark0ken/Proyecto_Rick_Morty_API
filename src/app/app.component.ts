4f1b6cc1dda1d9424af2877db88555eecb07a8f7
unknown <jorgenaruma88@gmail.com>
2023-12-18 14:05:17 +0100

    mensaje al gusto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './service/api.service';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'RICK_MORTY_API';
+
+  data: any[] = [];
+
+  constructor(private apiService: ApiService) { }
+  ngOnInit(): void {
+    this.llenarData();
+  }
+
+  llenarData() {
+    this.apiService.getData().subscribe(data => {
+      this.data = data;
+      console.log(this.data);
+    })
+  }
+
 }
